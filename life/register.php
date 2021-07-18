25a825af6ebde42514c3bd7f6df2b8ce12050a02
Vicki1019 <65385694+Vicki1019@users.noreply.github.com>
2021-07-18 13:54:44 +0800

    Add files via upload

--- life/register.php
+++ life/register.php
@@ -1,16 +1,30 @@
 <?php
-    if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['passwd']) && isset($_POST['passwdck'])){
-        require_once 'conn.php';
-        require_once 'validate.php';
-        $name = validate($_POST['name']);
-        $email = validate($_POST['email']);
-        $passwd = validate($_POST['passwd']);
-        $passwdck = validate($_POST['passwdck']);
-        $sql = "INSERT INTO member_info VALUES ('', '1', '$name', '$email', '$passwd', '', '', '', '', '')";
-        if(!$conn->query($sql)){
-            echo "failure";
+require_once 'DBHelper.php';
+require_once 'validate.php';
+
+/** @var \DBHelper $dbHelper 存取與資料庫相關功能的物件 */
+$dbHelper = new DBHelper();
+
+if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['passwd']) && isset($_POST['passwdck'])) {
+
+    $name = validate($_POST['name']);
+    $email = validate($_POST['email']);
+    $passwd = validate($_POST['passwd']);
+    $passwdck = validate($_POST['passwdck']);
+
+    if (
+        !empty($name) && !empty($email) && !empty($passwd)
+    ) {
+        $emailCheck = $dbHelper->emailCheck($email);
+        if($emailCheck != true){
+            print("failure");
         }else{
-            echo "success";
+            $insertResult = $dbHelper->insert($name, $email, $passwd);
+            if ($insertResult == 1) {
+                print("success");
+            } else {
+                print("failure");
+            }
         }
     }
-?>
+}

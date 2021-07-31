0821d1b6ed04a1fa1cde7f90cdac7c84375c5ec2
410714558 <s1071455@gm.pu.edu.tw>
2021-07-31 20:05:13 +0800

    get pw

--- life/userset.php
+++ life/userset.php
@@ -7,31 +7,8 @@ $dbHelper = new DBHelper();
 
 //get使用者名字
 $email = "SELECT 'email' FROM member_info";
-$sql = "SELECT 'member_nickname' FROM member_info WHERE email='$email'";
+$sql_n = "SELECT 'member_nickname' FROM member_info WHERE email='$email'";
 
-
-
-//修改使用者名字
-if (isset($_POST["action"]) && $_POST["action"] == 'update') {
-
-     $newName = $_POST['member_nickname'];
-     
-     $sql = "UPDATE 'member_info' SET 'member_nickname' = '$newName' ";
-     mysqli_query($db_link,$sql_query);
-     if (mysqli_affected_rows($link)>0) 
-     {
-          echo "資料已更新";
-     }
-     elseif(mysqli_affected_rows($link)==0) 
-     {
-          echo "無資料更新";
-     }
-     else 
-     {
-          echo "{$sql} 語法執行失敗，錯誤訊息: " . mysqli_error($link);
-     }
-     $db_link->close();
-
-     //header('Location: index.php');
-}
+//get使用者舊密碼
+$sql_pw = "SELECT 'passpw' FROM member_info WHERE email='$email'";
 ?>

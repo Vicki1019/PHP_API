a69ccf9322fc4c46705812e46e09371f7af6bec7
410714558 <s1071455@gm.pu.edu.tw>
2021-07-31 16:25:17 +0800

    revise userset

--- userset.php
+++ userset.php
@@ -6,21 +6,41 @@ require_once 'validate.php';
 $dbHelper = new DBHelper();
 
 //get使用者名字
-$con = mysqli_connect ( "localhost", "root", "1qaz2wsX" ); 
-$sql = "SELECT nickname FROM member_info";
-$result = mysqli_query($con, $sql);
-printf($row ["nickname"]);
+//$con = mysqli_connect ( "localhost", "root", "1qaz2wsX" ); 
+$sql = "SELECT 'member_nickname' FROM member_info WHERE email='$email'";
+//$result = mysqli_query($con, $sql);
+mysqli_query($this->connect, $sql);
+$result = $this->connect->affected_rows;
+if($result != 1)
+{
+     return false;
+}
+else
+{
+      return $result;
+}
 
 
-if (isset($_POST["action"]) && $_POST["action"] == 'update') {
+//if (isset($_POST["action"]) && $_POST["action"] == 'update') {
 
      $newName = $_POST['member_nickname'];
      
-     $sql_query = "UPDATE member_info SET member_nickname = '$newName' WHERE email = $email";
-
+     $sql = "UPDATE 'member_info' SET 'member_nickname' = '$newName' WHERE email ='$email'";
      mysqli_query($db_link,$sql_query);
+     if (mysqli_affected_rows($link)>0) 
+     {
+          echo "資料已更新";
+     }
+     elseif(mysqli_affected_rows($link)==0) 
+     {
+          echo "無資料更新";
+     }
+     else 
+     {
+          echo "{$sql} 語法執行失敗，錯誤訊息: " . mysqli_error($link);
+     }
      $db_link->close();
 
      //header('Location: index.php');
-}
+//}
 ?>
--- userset1.php
+++ userset1.php
@@ -5,13 +5,13 @@ require_once 'validate.php';
 /** @var \DBHelper $dbHelper 存取與資料庫相關功能的物件 */
 $dbHelper = new DBHelper();
 
-session_start (); 
+//session_start (); 
 $oldpassword = $_REQUEST ["oldpassword"]; 
 $newpassword = $_REQUEST ["newpassword"]; 
-$con = mysqli_connect ( "localhost", "root", "1qaz2wsX" ); 
+//$con = mysqli_connect ( "localhost", "root", "1qaz2wsX" ); 
 
 //選擇要更改資欄位的名字
-mysqli_select_db ( $con ,"passwd"); 
+//mysqli_select_db ( $con ,"passwd"); 
 $dbpassword = null; 
 $dbpassword = $row ["password"]; 
 if ($oldpassword != $dbpassword) 
@@ -20,6 +20,7 @@ if ($oldpassword != $dbpassword)
 }
 //如果上述使用者名稱密碼判定正確，則update進資料庫中
 $sql_query = " UPDATE member_info SET passwd = '$newpassword' WHERE email = $email " ; 
-mysqli_close ( $con ); 
+//mysqli_close ( $con ); 
+$db_link->close();
 print("密碼修改成功");
 ?> 

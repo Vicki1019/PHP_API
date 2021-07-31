4264c15eeaedcaa00dd68cf9343c1618ed2c8978
410714558 <s1071455@gm.pu.edu.tw>
2021-07-31 21:27:16 +0800

    revise

--- life/userset1.php
+++ life/userset1.php
@@ -4,7 +4,7 @@ require_once 'validate.php';
 
 /** @var \DBHelper $dbHelper 存取與資料庫相關功能的物件 */
 $dbHelper = new DBHelper();
-
+/*
 //修改暱稱
 if (isset($_POST["action"]) && $_POST["action"] == 'update') 
 {
@@ -26,32 +26,40 @@ if (isset($_POST["action"]) && $_POST["action"] == 'update')
          echo "{$sql} 語法執行失敗，錯誤訊息: " . mysqli_error($link);
     }*/
     $db_link->close();
-}
+//}
 
 //修改密碼 
-/*if (isset($_POST["action"]) && $_POST["action"] == 'update') 
-{
+$newpw = $_POST['passwd'];
+$newpw2 = $_POST['passwd2'];
+//if (isset($_POST["action"]) && $_POST["action"] == 'update') 
+//{
 
-    $newpw = $_POST['passwd'];
+    //$newpw = $_POST['passwd'];
     if ($oldpassword != $dbpassword) 
     { 
         print("密碼錯誤"); 
     }
-    
-    $sql = "UPDATE 'member_info' SET 'passwd' = '$newpw' ";
-    mysqli_query($db_link,$sql_query);
-    if (mysqli_affected_rows($link)>0) 
-    {
-         echo "資料已更新";
+    if ($newpw != $newpw2) 
+    { 
+        print("新密碼錯誤"); 
     }
-    elseif(mysqli_affected_rows($link)==0) 
+    else
     {
-         echo "無資料更新";
-    }
-    else 
-    {
-         echo "{$sql} 語法執行失敗，錯誤訊息: " . mysqli_error($link);
+        $sql = "UPDATE 'member_info' SET 'passwd' = '$newpw' ";
+        mysqli_query($db_link,$sql_query);
+        if (mysqli_affected_rows($link)>0) 
+        {
+            echo "資料已更新";
+        }
+        elseif(mysqli_affected_rows($link)==0) 
+        {
+            echo "無資料更新";
+        }
+        else 
+        {
+            echo "{$sql} 語法執行失敗，錯誤訊息: " . mysqli_error($link);
+        }
     }
     $db_link->close();
-}*/
+//}
 ?> 

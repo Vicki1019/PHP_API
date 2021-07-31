ef6c0ff62ef228923742496d39ab1321147b9289
410714558 <s1071455@gm.pu.edu.tw>
2021-07-31 22:16:29 +0800

    re

--- life/userset1.php
+++ life/userset1.php
@@ -28,7 +28,7 @@ if (isset($_POST["action"]) && $_POST["action"] == 'update')
 
 
 //修改密碼 
-if (isset($_POST["action"]) && $_POST["action"] == 'update') 
+if (isset($_POST["passwd"]) && $_POST["passwd"] == 'update') 
 {
     $email = "SELECT 'email' FROM member_info";
     $dbpw = "SELECT 'passwd' FROM member_info";
@@ -55,6 +55,6 @@ if (isset($_POST["action"]) && $_POST["action"] == 'update')
             echo "{$sql} 語法執行失敗，錯誤訊息: " . mysqli_error($link);
         }
     }
-    $db_link->close();
+    //$db_link->close();
 }
 ?> 

220bd62dc45cbe1265f06e7d890dbffbf8649d2f
410714558 <s1071455@gm.pu.edu.tw>
2021-07-31 23:18:02 +0800

    re

--- life/userset1.php
+++ life/userset1.php
@@ -4,14 +4,14 @@ require_once 'validate.php';
 
 /** @var \DBHelper $dbHelper 存取與資料庫相關功能的物件 */
 $dbHelper = new DBHelper();
-/*
+
 //修改暱稱
 $newName = $_POST['member_nickname'];
-if (isset($_POST["action"]) && $_POST["action"] == 'update') 
-{
+//if (isset($_POST["action"]) && $_POST["action"] == 'update') 
+//{
     
     $sql = "UPDATE 'member_info' SET 'member_nickname' = '$newName' ";
-    mysqli_query($db_link,$sql_query);
+    //mysqli_query($db_link,$sql_query);
     if (mysqli_affected_rows($link)>0) 
     {
          echo "資料已更新";
@@ -24,11 +24,11 @@ if (isset($_POST["action"]) && $_POST["action"] == 'update')
     {
          echo "{$sql} 語法執行失敗，錯誤訊息: " . mysqli_error($link);
     }
-}*/
+//}
 
 
 //修改密碼 
-if (isset($_POST["passwd"]) && $_POST["passwd"] == 'update') 
+/*if (isset($_POST["passwd"]) && $_POST["passwd"] == 'update') 
 {
     $email = "SELECT 'email' FROM member_info";
     $dbpw = "SELECT 'passwd' FROM member_info";
@@ -56,5 +56,5 @@ if (isset($_POST["passwd"]) && $_POST["passwd"] == 'update')
         }
     }
     //$db_link->close();
-}
+}*/
 ?> 

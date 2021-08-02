9b3b42b4c380df3332c1514546dd5d42c05a2b25
410714558 <s1071455@gm.pu.edu.tw>
2021-08-02 10:14:23 +0800

    re

--- life/userset1.php
+++ life/userset1.php
@@ -27,7 +27,7 @@ if (isset($_POST["account_name"]) )
 
 
 //修改密碼 
-if (isset($_POST["oldpwd"]) && $_POST["newpwd"] == 'update') 
+if (isset($_POST["oldpwd"]) && isset($_POST["newpwd"])) 
 {
     $email = "SELECT 'email' FROM member_info";
     $dbpw = "SELECT 'passwd' FROM member_info";

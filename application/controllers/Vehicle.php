df9c6e01625143ef15f8e5d799d9f85eb39fb9c6
Vicki <s1071930@gm.pu.edu.tw>
2021-10-27 13:29:41 +0800

    [新增]檢查使用者是否已填過載具

--- application/controllers/Vehicle.php
+++ application/controllers/Vehicle.php
@@ -10,6 +10,26 @@ class Vehicle extends CI_Controller
 			$this->load->model('Vehicle_model');
 	}
 
+    public function vehicle_ck(){
+        $params = (object)[
+            'email' => $this->input->post('email')
+        ];
+        $result = $this->Vehicle_model->vehicle_ck($params);
+        if($result == false){
+            print "failure";
+        }else{
+            foreach ($result as $row => $v){
+                $vehicleck['vehicle'][] = [
+                     'response' => 'success',
+                     'barcode' => $v['m_barcode'],
+                 ];
+             }
+             $this->output->set_content_type('application/json');
+             $this->output->set_output(json_encode($vehicleck
+             , JSON_UNESCAPED_UNICODE));
+        }
+    }
+
     public function update_barcode()
     {
         $params = (object)[
--- application/models/Vehicle_model.php
+++ application/models/Vehicle_model.php
@@ -9,6 +9,16 @@ class Vehicle_model extends CI_Model
         $this->load->database();
     }
 
+    public function vehicle_ck($params){
+        $sql = "SELECT m_barcode FROM member_info WHERE email=?";
+        $query = $this->db->query($sql, [$params->email]);
+        if ($query->num_rows() > 0) {
+            return $query->result_array();
+        } else {
+            return false;
+        }        
+    }
+
     public function update_barcode($params)
     {
         $sql = "UPDATE member_info SET m_barcode=? WHERE email=?";

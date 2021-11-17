5de815ce5535cb86b76ba75d4adb8a57b72205f4
ting20121108 <ting20121108@gmail.com>
2021-11-17 14:12:37 +0800

    推播串接後跳轉提示

--- application/views/LineToken.php
+++ application/views/LineToken.php
@@ -5,25 +5,11 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
-        <!-- <script src="LineNotify.js"> </script> -->
-        <?php
-            // include_once("application\controllers\LineNotify.php");
-            // $lineNotify = new lineNotify();
-            // $code = $_GET['code'];
-            // $token = $lineNotify->GetToken($code);
-            // print $token;
-            // http://192.168.1.213/PHP_API/index.php/LineNotify/LineAuthorize?email=ting20121108@gmail.com
-        ?>
-        <!-- <script>
-            $.ajax({
-                type:'POST',
-                url:"http://172.16.1.44/PHP_API/index.php/LineNotify/GetAuthorizeCode",
-                data:{ token: },
-                success: function (result){
-                    $('#response').show().html(result);
-                }
-            })
-        </script> -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.1.10/dist/sweetalert2.min.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.1.10/dist/sweetalert2.all.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </head>
     <body>
         <?php
@@ -57,7 +43,22 @@
                             if (result !== "success") {
                                 alert('串接失敗');
                             } else {
-                                alert('串接成功');
+                                Swal.fire({
+                                    title: '<strong>串接成功！</strong>',
+                                    icon: 'info',
+                                    html:
+                                        '恭喜，已成功串接生活冰館，<br>' +
+                                        '我們還需要您把他帶進群組，<br>' +
+                                        '這樣他才能盡責的提醒^^~',
+                                    confirmButtonText: '好的, 跳轉到Line',
+                                    showCancelButton: true,
+                                    cancelButtonText: '回生活冰館'
+                                }).then((result) => {
+                                    if (result.isConfirmed) {
+                                        uri = 'https://line.me/R/nv/chat';
+                                        window.location.href = uri;
+                                    }
+                                })
                             }
                         }
                     })

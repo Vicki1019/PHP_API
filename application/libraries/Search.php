ae01d1cdf36b65b6bfd70d058aa7b7c13e713c07
Vicki <s1071930@gm.pu.edu.tw>
2021-12-22 23:59:28 +0800

    爬蟲

--- application/libraries/Search.php
+++ application/libraries/Search.php
@@ -4,7 +4,7 @@ if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 // require_once APPPATH."/third_party/simple_html_dom.php";
 require_once APPPATH.'third_party\google-search-results.php';
-// require_once APPPATH.'third_party\restclient.php';
+ require_once APPPATH.'third_party\restclient.php';
 
 class Search extends SerpApiSearch {
     public function __construct() {
